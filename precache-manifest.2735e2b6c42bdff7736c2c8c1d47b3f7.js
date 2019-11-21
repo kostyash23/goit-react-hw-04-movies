9d2ee1ba2e083aa51915d259a65f8efff3cf8eeb
Kotya Shmotoloha <kostiashmotoloha@gmaol.com>
2019-11-21 14:07:50 +0200

    Updates

---
+++ precache-manifest.2735e2b6c42bdff7736c2c8c1d47b3f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b5525ce2440514ce2eac477fd58d81",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "efbd34ab9805c6f25404",
+    "url": "/goit-react-hw-04-movies/static/css/main.3cbd41dc.chunk.css"
+  },
+  {
+    "revision": "ec3bcad2c20ddf991811",
+    "url": "/goit-react-hw-04-movies/static/js/2.b4c977b2.chunk.js"
+  },
+  {
+    "revision": "efbd34ab9805c6f25404",
+    "url": "/goit-react-hw-04-movies/static/js/main.9727fccf.chunk.js"
+  },
+  {
+    "revision": "2a346caa80309ec332ae",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.55d60aea.js"
+  }
+]);
